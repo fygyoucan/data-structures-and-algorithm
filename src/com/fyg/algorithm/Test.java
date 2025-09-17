6470f41ed54503fbca68fe938d6368d519cd3eef
fangdou <2516330531@qq.com>
2025-09-17 21:45:46 +0800

    my first commit

---
+++ src/com/fyg/algorithm/Test.java
@@ -0,0 +1,9 @@
+package com.fyg.algorithm;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈");
+    }
+}
